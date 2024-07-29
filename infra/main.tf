cd86aad88f676d4cf04598aa618c25a9e5c6d2ca
Alex Vaccarino <avacc@alexs-mbp.lan>
2024-07-29 13:30:58 -0400

    Initial commit

---
+++ infra/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.9.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  version = "5.60"
+}
